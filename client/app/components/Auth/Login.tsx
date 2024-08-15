38c868c4f8c6d504405411d3ef0905f0004ae3c7
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-08-15 22:21:25 +0100

    update

--- client/app/components/Auth/Login.tsx
+++ client/app/components/Auth/Login.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { FC, useState } from "react";
 
 import { useFormik } from "formik";
 
@@ -14,7 +15,26 @@ type Props = {
   setRoute: (route: string) => void;
 };
 
-const Login = (props: Props) => {
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email")
+    .required("Please enter your email"),
+  password: Yup.string().required("Please enter your password"),
+});
+
+const Login: FC<Props> = (Props) => {
+  const [show, setShow] = useState(false);
+
+  const formik = useFormik({
+    initialValues: { email: "", password: "" },
+    validationSchema: schema,
+    onSubmit: async ({ email, password }) => {
+      console.log(email, password);
+    },
+  });
+
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
+
   return <div></div>;
 };
 
