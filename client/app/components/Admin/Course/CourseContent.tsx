9365172ca8679968b359222d3305de234962175b
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-08-23 21:43:16 +0100

    upate

--- client/app/components/Admin/Course/CourseContent.tsx
+++ client/app/components/Admin/Course/CourseContent.tsx
@@ -1,4 +1,8 @@
+import { styles } from "@/app/styles/style";
 import React, { FC, useState } from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { BsPencil } from "react-icons/bs";
 
 type Props = {
   active: number;
@@ -26,6 +30,19 @@ const CourseContent: FC<Props> = ({
     e.preventDefault();
   };
 
+  const handleCollapseToggle = (index: number) => {
+    const updatedCollapsed = [...isCollapsed];
+    updatedCollapsed[index] = !updatedCollapsed[index];
+    setIsCollapsed(updatedCollapsed);
+  };
+
+  const handleRemoveLink = (index: number, linkIndex: number) => {
+    const updatedData = [...courseContentData];
+
+    updatedData[index].links.splice(linkIndex, 1);
+    setCourseContentData(updatedData);
+  };
+
   return (
     <div className="w-[80%] m-auto mt-24 p-3">
       <form action="" onSubmit={handleSubmit}>
@@ -39,7 +56,171 @@ const CourseContent: FC<Props> = ({
                 className={`w-full bg-[#cdc8c817] p-4 ${
                   showSectionInput ? "mt-10" : "mb-0"
                 }`}
-              ></div>
+              >
+                {showSectionInput && (
+                  <>
+                    <div className="flex w-full items-center">
+                      <input
+                        type="text"
+                        className={`
+      
+      text-[20px] ${
+        item.videoSection === "Untitled section" ? "w-[170px]" : "w-max"
+      }
+      font-Poppins cursor-pointer dark:text-white text-black bg-transparent outline-none
+      `}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].videosection = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+                      <BsPencil className="curosr-pointer dark:text-white text-black" />
+                    </div>
+                    <br />
+                  </>
+                )}
+
+                <div className="flex w-full items-center justify-between my-0">
+                  {isCollapsed[index] ? (
+                    <>
+                      {item.title ? (
+                        <p className="font-Poppins dark:text-white text-black">
+                          {item + 1}. {item.title}
+                        </p>
+                      ) : (
+                        <></>
+                      )}
+                    </>
+                  ) : (
+                    <div></div>
+                  )}
+
+                  {/* arrow button for collapsed video content  */}
+
+                  <div className="flex items-center">
+                    <AiOutlineDelete
+                      className={`dark:text-white text-[20px] mr-2 text-black ${
+                        index > 0 ? "cursor-pointer" : "cursor-no-drop"
+                      }`}
+                      onClick={() => {
+                        if (index > 0) {
+                          const updatedData = [...courseContentData];
+                          updatedData.splice(index, 1);
+                          setCourseContentData(updatedData);
+                        }
+                      }}
+                    />
+
+                    <MdOutlineKeyboardArrowDown
+                      fontSize="large"
+                      className="dark:text-white text-black"
+                      style={{
+                        transform: isCollapsed[index]
+                          ? "rotate(180deg)"
+                          : "rotate(0deg)",
+                      }}
+                      onClick={() => handleCollapseToggle(index)}
+                    />
+                  </div>
+                </div>
+                {!isCollapsed[index] && (
+                  <>
+                    <div className="my-3">
+                      <label htmlFor="" className={styles.label}>
+                        Video Title
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Project Plan"
+                        className={`${styles.input}`}
+                        value={item.title}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].title = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="" className={styles.label}>
+                        Video URL
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="sdder"
+                        className={`${styles.input}`}
+                        value={item.videoUrl}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].videoUrl = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+                    </div>
+
+                    <div className="mb-3">
+                      <label htmlFor="" className={styles.label}>
+                        Video description
+                      </label>
+                      <textarea
+                        rows={8}
+                        cols={30}
+                        className={`${styles.input} !h-min py-2`}
+                        placeholder="sddr"
+                        value={item.description}
+                        name=""
+                        id=""
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].description = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      ></textarea>
+                      <br />
+                      <br />
+                      <br />
+                    </div>
+                    {item?.links.map((link: any, linkIndex: number) => (
+                      <div className="mb-3 block">
+                        <div className="w-full flex items-center justify-center ">
+                          <label className={styles.label} htmlFor="">
+                            Link {linkIndex + 1}
+                          </label>
+
+                          <AiOutlineDelete
+                            className={`${
+                              linkIndex === 0
+                                ? "cursor-no-drop"
+                                : "cursor-pointer"
+                            }
+                          text-black dark:text-white text-[20px]
+                          `}
+                            onClick={() =>
+                              linkIndex === 0
+                                ? null
+                                : handleRemoveLink(index, linkIndex)
+                            }
+                          />
+                        </div>
+                        <input
+                          type="text"
+                          placeholder="Source code...(Link title)"
+                          className={`${styles.input}`}
+                          value={link.title}
+                          onChange={(e) => {
+                            const updatedData = [...courseContentData];
+                            updatedData[index].links[linkIndex].title =
+                              e.target.value;
+                            setCourseContentData(updatedData);
+                          }}
+                        />
+                      </div>
+                    ))}
+                  </>
+                )}
+              </div>
             </>
           );
         })}
