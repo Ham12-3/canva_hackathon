45d14e8d4156926ca862d223b8be9f56e70f878b
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-06 18:18:02 +0100

    update

--- client/app/components/Course/CourseCard.tsx
+++ client/app/components/Course/CourseCard.tsx
@@ -1,6 +1,9 @@
+import Ratings from "@/app/utils/Ratings";
+
 import Image from "next/image";
 import Link from "next/link";
 import React, { FC } from "react";
+import { AiOutlineUnorderedList } from "react-icons/ai";
 
 type Props = {
   item: any;
@@ -18,10 +21,40 @@ const CourseCard: FC<Props> = ({ item, isProfile }) => {
           width={500}
           height={300}
           objectFit="cover"
-          className="rounded-lg"
+          className="rounded w-full"
           alt=""
         />
         <br />
+        <h1 className="font-Poppins text-[16px] text-black dark:text-[#fff]">
+          {item.name}
+        </h1>
+        <div className="w-full flex items-center justify-between pt-2">
+          <Ratings rating={item.ratings} />
+          <h5
+            className={`text-black dark:text-[#fff] ${
+              isProfile && "hidden 800px:inline"
+            }`}
+          >
+            {item.purchased} Students
+          </h5>
+        </div>
+        <div className="w-full flex items-center justify-between pt-3">
+          <div className="flex">
+            <h3 className="text-black dark:text-[#fff]">
+              {item.price === 0 ? "Free" : "£" + item.price}
+            </h3>
+            <h5 className="pl-3 text-[14px] mt-[-5px] line-through opacity-80 text-black dark:text-[#fff]">
+              {"£" + item.estimatedPrice}
+            </h5>
+          </div>
+          <div className="flex items-center pb-3">
+            <AiOutlineUnorderedList size={20} fill="#fff" />
+            <h5 className="pl-2 text-black dark:text-[#fff]">
+              {item.courseData?.length}{" "}
+              {item.courseData?.length > 1 ? "Lectures" : "Lecture"}
+            </h5>
+          </div>
+        </div>
       </div>
     </Link>
   );
--- client/app/components/Route/Courses.tsx
+++ client/app/components/Route/Courses.tsx
@@ -13,22 +13,24 @@ const Courses = (props: Props) => {
   }, [data]);
 
   return (
-    <div className={`w-[90%] 800px:w-[80%] m-auto`}>
-      <h1 className="text-center font-Poppins text-[25px] leading-[35px] sm:text-3xl lg:text-4xl dark:text-white text-[#000] font-[700] 800px:leading-[40px]">
-        Expand Your Career{" "}
-        <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
-          Opportunity
-        </span>
+    <div className="relative flex flex-col items-center justify-center py-16 lg:py-20 bg-white dark:bg-black text-black dark:text-white">
+      <div className="w-[90%] 800px:w-[80%] m-auto">
+        <h1 className="text-center font-Poppins text-[25px] leading-[35px] sm:text-3xl lg:text-4xl dark:text-white text-[#000] font-[700] 800px:leading-[40px]">
+          Expand Your Career{" "}
+          <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+            Opportunity
+          </span>
+          <br />
+          With Our Courses
+        </h1>
         <br />
-        With Our Courses
-      </h1>
-      <br />
-      <br />
-      <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 mg:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
-        {courses &&
-          courses.map((item: any, index: number) => (
-            <CourseCard item={item} key={index} />
-          ))}
+        <br />
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 lg:gap-[25px] lg:grid-cols-3 xl:grid-cols-4 mb-12">
+          {courses &&
+            courses.map((item: any, index: number) => (
+              <CourseCard item={item} key={index} />
+            ))}
+        </div>
       </div>
     </div>
   );
