f75787af5b387f560a3ad994653696081f7719dd
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-10 07:10:19 +0100

    update

--- client/app/components/Course/CourseContentList.tsx
+++ client/app/components/Course/CourseContentList.tsx
@@ -1,9 +1,11 @@
 import React, { FC, useState } from "react";
+import { BsChevronDown, BsChevronUp } from "react-icons/bs";
 
 type Props = {
   data: any;
   activeVideo?: number;
   setActiveVideo?: any;
+  isDemo?: boolean;
 };
 
 const CourseContentList: FC<Props> = (props) => {
@@ -27,7 +29,56 @@ const CourseContentList: FC<Props> = (props) => {
     }
     setVisibleSections(newVisibleSections);
   };
-  return <div>CourseContentList</div>;
+  return (
+    <div
+      className={`mt-[15px] w-full ${
+        !props.isDemo && "ml-[-30px] min-h-screen sticky top-24 left-0 z-30"
+      }`}
+    >
+      {videoSections.map((section: string, sectionIndex: number) => {
+        const isSectionVisible = visibleSections.has(section);
+        const sectionVideos: any[] = props.data.filter(
+          (item: any) => item.videoSection === section
+        );
+        const sectionVideoCount: number = sectionVideos.length;
+        const sectionVideoLength: number = sectionVideos.reduce(
+          (totalLength: number, item: any) => totalLength + item.videoLength,
+          0
+        );
+
+        const sectionStartIndex: number = totalCount;
+        totalCount += sectionVideoCount;
+
+        const sectionContentHours: number = sectionVideoLength / 60;
+
+        return (
+          <div
+            className={`${!props.isDemo && "border-b border-[#ffffff8e] pb-2"}`}
+            key={section}
+          >
+            <div className="w-full flex">
+              {/* Render video section  */}
+              <div className="w-full flex justify-between items-center">
+                <h2 className="text-[22px] text-black dark:text-white">
+                  {section}
+                </h2>
+                <button
+                  className="mr-4 cursor-pointer text-black dark:text-white"
+                  onClick={() => toggleSection(section)}
+                >
+                  {isSectionVisible ? (
+                    <BsChevronUp size={20} />
+                  ) : (
+                    <BsChevronDown size={20} />
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CourseContentList;
