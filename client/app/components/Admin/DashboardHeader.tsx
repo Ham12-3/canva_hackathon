1ee7caa4fcf1de1dfb3ce0bd8601c0a818f19426
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-14 14:31:45 +0100

    upate

--- client/app/components/Admin/DashboardHeader.tsx
+++ client/app/components/Admin/DashboardHeader.tsx
@@ -4,10 +4,12 @@ import {
   useGetAllNotificationsQuery,
   useUpdateNotificationStatusMutation,
 } from "@/redux/features/notifications/notificationsApi";
-import React, { FC, use, useState } from "react";
+import React, { FC, use, useEffect, useState } from "react";
 import { IoMdNotificationsOutline } from "react-icons/io";
 
 import socketIO from "socket.io-client";
+import { format } from "timeago.js";
+
 const ENDPOINT = process.env.NEXT_PUBLIC_SOCKET_SERVER_URI || "";
 
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
@@ -36,6 +38,28 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
   const playerNotificationSound = () => {
     audio.play();
   };
+  useEffect(() => {
+    if (data) {
+      setNotifications(
+        data.notifications.filter((item: any) => item.status === "unread")
+      );
+    }
+    if (isSuccess) {
+      refetch();
+    }
+    audio.load();
+  }, [data, isSuccess]);
+
+  useEffect(() => {
+    socketId.on("newNotification", (data) => {
+      refetch();
+      playerNotificationSound();
+    });
+  }, []);
+
+  const handleNotificationStatusChange = async (id: string) => {
+    await updateNotificationStatus(id);
+  };
 
   // Function to toggle the notification panel
 
@@ -49,7 +73,7 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
       >
         <IoMdNotificationsOutline className="text-2xl dark:text-white text-black cursor-pointer" />
         <span className="absolute -top-2 -right-2 bg-[#3ccba0] rounded-full w-[20px] h-[20px] text-[12px] flex items-center justify-center text-white">
-          3
+          {notifications && notifications.length}
         </span>
       </div>
 
@@ -59,41 +83,27 @@ const DashboardHeader: FC<Props> = ({ open, setOpen }) => {
             Notifications
           </h5>
 
-          <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f] ">
-            <div className="w-full flex items-center justify-between p-2">
-              <p className="text-black dark:text-white">
-                New Question Received
-              </p>
-              <p className="text-black dark:text-white cursor-pointer">
-                Mark as read
-              </p>
-            </div>
-            <p className="px-2 text-black dark:text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quod.
-            </p>
-            <p className="p-2 text-black dark:text-white text-[14px]">
-              5 days ago
-            </p>
-          </div>
-
-          <div className="dark:bg-[#2d3a4ea1] bg-[#00000013] font-Poppins border-b dark:border-b-[#ffffff47] border-b-[#0000000f]">
-            <div className="w-full flex items-center justify-between p-2">
-              <p className="text-black dark:text-white">
-                New Question Received
-              </p>
-              <p className="text-black dark:text-white cursor-pointer">
-                Mark as read
-              </p>
-            </div>
-            <p className="px-2 text-black dark:text-white">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-              quod.
-            </p>
-            <p className="p-2 text-black dark:text-white text-[14px]">
-              5 days ago
-            </p>
-          </div>
+          {notifications &&
+            notifications.map((item: any, index: number) => (
+              <div
+                className="dark:bg-[#2d3a4e] bg-[#00000013] font-Poppins dark:border-b-[#ffffff47] border-b-[#0000000f]
+              
+              "
+              >
+                <div className="w-full flex items-center justify-between p-2">
+                  <p className="text-black dark:text-white">{item.title}</p>
+                  <p className="px-2 text-black dark:text-white cursor-pointer">
+                    mark as read
+                  </p>
+                </div>
+                <p className="px-2 text-black dark:text-white">
+                  {item.message}
+                </p>
+                <p className="p-2 text-black dark:text-white text-[14px]">
+                  {format(item.createdAt)}
+                </p>
+              </div>
+            ))}
         </div>
       )}
     </div>
