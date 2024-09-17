658296a370ff3dfc23628e35d907e7586f06e1f1
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-17 21:00:56 +0100

    update

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,17 +1,7 @@
-import { NextConfig } from "next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com", "randomuser.me"],
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      type: "asset/resource",
-    });
-
-    return config;
   },
 };
 
