71ca401c237f612dcad3ea3018d6d1051e42da43
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-18 10:35:04 +0100

    update

--- client/app/course-access/[id]/page.tsx
+++ client/app/course-access/[id]/page.tsx
@@ -6,6 +6,7 @@ import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 import { useGetCourseContentQuery } from "@/redux/features/courses/coursesApi";
 import { redirect } from "next/navigation";
 import React, { useEffect } from "react";
+
 type Props = {
   params: any;
 };
@@ -13,35 +14,43 @@ type Props = {
 const Page = ({ params }: Props) => {
   const id = params.id;
   const { isLoading, error, data } = useLoadUserQuery(undefined, {});
-  const { data: contentData, isLoading: supportLoader } =
-    useGetCourseContentQuery(id);
-  console.log(contentData, "contentData");
-  console.log(data.user.courses.courseId, "data");
+  const {
+    data: contentData,
+    isLoading: contentLoading,
+    error: contentError,
+  } = useGetCourseContentQuery(id);
+
+  console.log(contentData, "contentData"); // This will now only log when contentData is defined
+  console.log(data?.user?.courses?.courseId, "data");
   console.log(id, "id");
+
   useEffect(() => {
-    if (data) {
-      const isPurchased = data.user.courses.find(
+    if (data && !isLoading) {
+      const isPurchased = data.user?.courses?.find(
         (course: any) => course.courseId === id
       );
-      if (!isPurchased) {
-        redirect("/");
-      }
-      if (error) {
+
+      // Only redirect if data is loaded and there's no purchase
+      if (!isPurchased || error) {
         redirect("/");
       }
     }
-  }, [data, error]);
+  }, [data, error, isLoading, id]);
+
+  // Display loader while any data is loading
+  if (isLoading || contentLoading) {
+    return <Loader />;
+  }
+
+  // Handle case where contentData or user data is missing or API returned an error
+  if (contentError || !contentData || !data || !data.user) {
+    return <div>Error: Course content or user data not available.</div>;
+  }
 
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div>
-          <CourseContent id={id} user={data.user} />
-        </div>
-      )}
-    </>
+    <div>
+      <CourseContent id={id} user={data.user} />
+    </div>
   );
 };
 
