89750c24840a8d6a9c5f4709b71d1a5184a6ac7a
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-23 19:09:29 +0100

    update

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -44,7 +44,7 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
   });
 
   useEffect(() => {
-    if (!userData && sessionData && !logout) {
+    if (!userData && sessionData && logout) {
       socialAuth({
         email: sessionData?.user?.email,
         name: sessionData?.user?.name,
